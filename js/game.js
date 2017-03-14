56da52cebbb255b2420cd2b0f8e2c0efd21a50bf
Eduardo Heinen <eduardo.heinen@eduardo-heinen.ad.deliveryhero.com>
2017-03-14 13:41:50 +0100

    Included timer, headshots score and compressed images

--- js/game.js
+++ js/game.js
@@ -5,38 +5,11 @@ window.onload = function() {
     canvas.addEventListener("click", onClick, false);
 
     function onClick(e) {
-        //click_x = e.pageX - 232;
-        click_x = e.pageX - 321;
-        //click_y = e.pageY - 60;
-        click_y = e.pageY - 62;
-        pigeon_size = [pigeon_x, pigeon_x + 82, pigeon_y, pigeon_y + 75]
-
-        if ((click_x >= pigeon_size[0] && click_x <= pigeon_size[1]) &&
-            (click_y >= pigeon_size[2] && click_y <= pigeon_size[3])) {
-            score += 1;
-        } else {
-            life_remains -= 1;
-            renderLifeRemains();
-            if(life_remains <= 0){
-                buildGameOver();
-            }
-            if((click_x >= 350 && click_x <= 425) && (click_y >= 262 && click_y <= 274)){
-                reset();
-                initialize();
-                pigeonFly();
-            }
-        }
-
-        if(life_remains > 0){
-            renderBackground();
-            renderTop();
-            pigeonFly();
-        }
+        shoot(e);
     }
 };
 
 function pigeonFly() {
-    // Pigeon image:
     var pigeonImage = new Image();
     pigeonImage.src = "imgs/pigeon.png";
 
@@ -50,4 +23,63 @@ function pigeonFly() {
 
 function randomIntFromInterval(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function shoot(eventClick) {
+    clickPositions = getMousePosition(eventClick);
+    click_x = clickPositions.x;
+    click_y = clickPositions.y;
+
+    pigeon_size = [pigeon_x, pigeon_x + 58, pigeon_y + 21, pigeon_y + 70]
+    pigeon_head = [pigeon_x + 59, pigeon_x + 82, pigeon_y, pigeon_y + 20]
+
+    if ((click_x >= pigeon_head[0] && click_x <= pigeon_head[1]) &&
+        (click_y >= pigeon_head[2] && click_y <= pigeon_head[3])) {
+        score += 3;
+    }
+    else if ((click_x >= pigeon_size[0] && click_x <= pigeon_size[1]) &&
+        (click_y >= pigeon_size[2] && click_y <= pigeon_size[3])) {
+        score += 1;
+    } else {
+        life_remains -= 1;
+        renderLifeRemains();
+        if (life_remains <= 0) {
+            buildGameOver();
+        }
+        if (gameOver) {
+            if ((click_x >= 340 && click_x <= 435) && (click_y >= 252 && click_y <= 284)) {
+                reset();
+                initialize();
+                pigeonFly();
+            }
+        }
+    }
+
+    if (life_remains > 0) {
+        renderBackground();
+        renderTop();
+        pigeonFly();
+    }
+}
+
+function getMousePosition(evt) {
+    var rect = canvas.getBoundingClientRect();
+    return {
+        x: evt.clientX - rect.left,
+        y: evt.clientY - rect.top
+    };
+}
+
+function msToMMSS(ms) {
+    var seconds = ms / 1000;
+    var minutes = parseInt(seconds / 60).toString();
+    if (minutes.length < 2) {
+        minutes = "0" + minutes;
+    }
+
+    seconds = parseInt(seconds % 60).toString();
+    if (seconds.length < 2) {
+        seconds = "0" + seconds;
+    }
+    return (minutes + ":" + seconds);
 }
--- js/initialize.js
+++ js/initialize.js
@@ -8,14 +8,18 @@ time_remains = 0;
 life_remains = 3;
 pigeon_x = 0;
 pigeon_y = 0;
-gameOverBuilt = false;
+gameOver = false;
+startToPlay = new Date();
+timePlayed = "00:00";
 
-function reset(){
+function reset() {
     score = 0;
     velocity = 0;
     time_remains = 0;
     life_remains = 3;
-    gameOverBuilt = false;
+    gameOver = false;
+    startToPlay = new Date();
+    timePlayed = "00:00";
 }
 
 function initialize() {
@@ -23,11 +27,20 @@ function initialize() {
     renderTop();
 }
 
-function buildGameOver(){
-    if(gameOverBuilt == false){
+// Build game over:
+function buildGameOver() {
+    if (gameOver == false) {
         ctx.fillStyle = "#333";
         ctx.globalAlpha = 0.75;
         ctx.fillRect(0, 51, 1000, 1000);
+
+        ctx.fillStyle = "#FFF";
+        ctx.font = "30px sans-serif";
+        ctx.fillText("SCORE: " + score, (canvas.width / 2) - 350, (canvas.height / 2) - 125);
+
+        ctx.fillStyle = "#FFF";
+        ctx.font = "30px sans-serif";
+        ctx.fillText("TIME: " + timePlayed, (canvas.width / 2) + 185, (canvas.height / 2) - 125);
 
         ctx.fillStyle = "#FFF";
         ctx.font = "30px sans-serif";
@@ -37,11 +50,14 @@ function buildGameOver(){
         ctx.font = "18px sans-serif";
         ctx.fillText("Try Again", (canvas.width / 2) - 50, (canvas.height / 2) + 35);
 
-        gameOverBuilt = true;
+        renderTop();
+
+        gameOver = true;
     }
 }
 
-function buildScenario(){
+// Build scenario:
+function buildScenario() {
     canvas = document.getElementById("screen-pigeon");
     ctx = canvas.getContext("2d");
     gameWidth = 800;
@@ -54,24 +70,23 @@ function buildScenario(){
     renderTop();
 }
 
-function renderTop(){
-    // Build top line to get scores:
+// Render top line to get scores:
+function renderTop() {
     ctx.fillStyle = "#333";
     ctx.fillRect(0, 0, gameWidth, 50);
-
-    // Life remains label:
-    ctx.fillStyle = "#FFF";
-    ctx.font = "14px sans-serif";
-    ctx.fillText("Life: ", 20, 30);
 
     renderLifeRemains();
     renderScore();
     renderTime();
 }
 
-function renderLifeRemains(){
-    // Life remains value:
-    if(life_remains >= 0){
+// Render Life remains:
+function renderLifeRemains() {
+    ctx.fillStyle = "#FFF";
+    ctx.font = "14px sans-serif";
+    ctx.fillText("Life: ", 20, 30);
+
+    if (life_remains >= 0) {
         var lifeImage = new Image();
         lifeImage.src = "imgs/life_" + life_remains + ".png";
         lifeImage.onload = function() {
@@ -80,32 +95,39 @@ function renderLifeRemains(){
     }
 }
 
-function renderScore(){
-    // Score label:
+// Render Score label and value:
+function renderScore() {
     ctx.fillStyle = "#FFF";
     ctx.font = "14px sans-serif";
     ctx.fillText("Score: ", 150, 30);
 
-    // Score value:
     ctx.fillStyle = "#FFF";
     ctx.font = "14px sans-serif";
     ctx.fillText(score, 195, 31);
 }
 
-function renderTime(){
-    // Time label:
+// Render Time label and value:
+function renderTime() {
+    ctx.fillStyle = "#333";
+    ctx.fillRect(690, 10, 90, 31);
+
     ctx.fillStyle = "#FFF";
     ctx.font = "14px sans-serif";
     ctx.fillText("Time: ", 700, 30);
 
-    // Time value:
+    var timeNow = new Date();
+    timePlayed = msToMMSS(timeNow - startToPlay);
     ctx.fillStyle = "#FFF";
     ctx.font = "14px sans-serif";
-    ctx.fillText("00:00", 740, 31);
+    ctx.fillText(timePlayed, 740, 31);
+
+    if (!gameOver) {
+        requestAnimationFrame(renderTime);
+    }
 }
 
+// Render Background image:
 function renderBackground() {
-    // Background image:
     var bgImage = new Image();
     bgImage.src = "imgs/background.jpg";
     bgImage.onload = function() {
